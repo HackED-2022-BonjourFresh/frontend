421fd4b65c99956b21b8654643be43aa83faf34c
ApluUalberta <aplu@ualberta.ca>
2022-01-15 12:28:19 -0700

    Home page placeholder

--- pages/Home.js
+++ pages/Home.js
@@ -1,1 +1,23 @@
 // Boilerplate
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home(){
+    return (
+        <View style={StyleSheet.container}>
+            <Text>Page that contains the week pages for given recipes</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
