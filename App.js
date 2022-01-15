f4b74135456b8341c9c5ebabdd8b32a4616fab7b
ApluUalberta <aplu@ualberta.ca>
2022-01-15 12:42:38 -0700

    app.js and login

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Week from './pages/Login/Week.js';
+import Main from './pages/';
+import Menu from './Containers/Menu/Menu';
+import User from './Containers/User/User';
+import NutrientCard from './Components/NutrientCard';
+import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: '',
+            headerStyle: {
+              height: '0px'
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
