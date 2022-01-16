26f6c10b495e79791a84e113f9d7a9d776602cb1
Ryan Kang <rkang2009@gmail.com>
2022-01-15 23:30:41 -0500

    dsada

--- pages/WeekPlan.js
+++ pages/WeekPlan.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, ScrollView, Text, View } from 'react-native';
+import axios from 'axios';
+
+import RecipeCard from './Components/RecipeCard';
 
 export default function WeekPlan() {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:5000/recipes_for_user`)
+            .then(res => {
+                setRecipes(res.data);
+            });
+    }, []);
+
+    const onRecipeCardClicked = (i) => {
+        navigation.navigate('Recipe', { recipe: dishes[i] });
+    }
+
     return (
         <View>
-            <Text>Test</Text>
+            <ScrollView>
+                {
+                    recipes.map((d, i) => <RecipeCard
+                                                key={i}
+                                                title={d.recipe_name}
+                                                imageURL={d.image}
+                                                onClick={() => onRecipeCardClicked(i)}
+                                            />
+                    )
+                }
+            </ScrollView>
         </View>
     )
 }
