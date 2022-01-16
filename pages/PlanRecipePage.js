6fe1087f525f34c9b968926b2265ca19ba1af43a
ApluUalberta <aplu@ualberta.ca>
2022-01-15 22:46:09 -0700

    Plan Recipe page

---
+++ pages/PlanRecipePage.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView, Image } from 'react-native';
+
+import {ProgressButton} from 'react-native-progress-button';
+import axios from 'axios';
+
+
+const Recipe = ({navigation, route}) => {
+    const [dishesImage, setdishesImage] = useState(route.params.recipe.image);
+    const [date, setDate] = useState(route.params.recipe.date);
+    const [dishesName, setdishesName] = useState(route.params.recipe.title);
+    const [ingredients, setIngredients] = useState(route.params.recipe.ingredients);
+    const [instruction, setInstruction] = useState();
+    const [recipeName, setRecipeName] = useState(route.params.recipe.recipe_name);
+
+    useEffect(() => {
+
+        let instructions = route.params.recipe.instructions;
+
+        const splitInstructions = instructions.split("\n");
+        setInstruction(splitInstructions);
+    },[]);
+
+    return (
+        <View style={{marginLeft:20, marginRight:20}}>
+            <Image style={styles.recipeImage} source={dishesImage}/>
+            <Text style={{fontWeight:'800', fontSize: '40', fontStyle:"monospace"}}>{dishesName}</Text>
+            <Text style={{fontWeight:'300', fontSize: '5', fontStyle:"monospace"}}>{date}</Text>
+            
+            {
+                ingredients.map(ingredient => 
+                    <View>
+                        <Text style={{fontWeight:'700', paddingTop: 10}}>
+                            Ingredient {ingredient.name}
+                        </Text>
+                        <Text style={{paddingTop: 10}}>
+                            {ingredient.quantity}
+                        </Text>
+                    </View>
+                )
+            }
+
+        </View>
+    );
+}
+
+export default Recipe;
